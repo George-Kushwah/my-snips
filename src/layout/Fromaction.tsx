0f52fb86623f50d59acb62d0734c3b53fc0dbc41
George <rahulsapnaji@gmail.com>
2024-12-27 15:05:27 +0530

    add sonars

--- src/layout/Fromaction.tsx
+++ src/layout/Fromaction.tsx
@@ -8,7 +8,7 @@ export const Fromaction: any = async (prevSate: any, fromData: any) => {
   const sname = fromData.get("sname");
   await wait(800);
   if (sname) {
-    return { mess: "my name is " + sname };
+    return { mess: "my name is here " + sname };
   } else return { mess: "my name is wrong" };
 };
 
