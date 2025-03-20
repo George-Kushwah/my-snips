edacec7413986650944f6103d0c1e325b052ffcd
George <rahulsapnaji@gmail.com>
2025-03-20 08:31:12 +0530

    changes

--- src/layout/All-skills.tsx
+++ src/layout/All-skills.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useTransition } from "react";
+import React, { useState, useEffect, useTransition, useRef } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { Queryget } from "./../query/Queryfun";
 const Allskills = () => {
+  const refs = useRef<HTMLInputElement>(null);
   const [val, setval] = useState<any>(0);
   const [btns, setbtns] = useState<boolean>(false);
   const [arval, setarval] = useState<any>([
@@ -21,6 +22,17 @@ const Allskills = () => {
     "fri",
     "sat",
   ]);
+
+  const [car, setcar] = useState<any>([
+    "Maruti",
+    "Hyundai",
+    "jeep",
+    "Audi",
+    "Bike",
+    "Car",
+  ]);
+  const [finds, setfinds] = useState<any>("");
+  const [binimg, setbinimg] = useState<any>();
   const [ispendding, starttrans] = useTransition();
   const { error, isPending, data }: any = useQuery(Queryget());
   useEffect(() => {
@@ -45,7 +57,38 @@ const Allskills = () => {
       });
     });
   };
-  console.log(data);
+
+  useEffect(() => {
+    if (finds.length > 0) {
+      let a = car.filter((item: any) => {
+        return item.toLowerCase() === finds.toLowerCase();
+      });
+      if (a.length > 0) {
+        setcar((prev: any) => {
+          let update = [...prev];
+          console.log(update);
+          update = a;
+          return update;
+        });
+      }
+    }
+  }, [finds]);
+  //   console.log(data);
+
+  const Fileupload = (ev: any) => {
+    let file = ev.target.files[0];
+    console.log(file);
+    let fromdata: any = new FileReader();
+    fromdata.onload = () => {
+      setbinimg(fromdata.result);
+    };
+    fromdata.readAsDataURL(file);
+    // for clear value in file
+    // let dc = refs.current;
+    // if (dc) {
+    //   dc.value = "";
+    // }
+  };
 
   return (
     <div className="container-fluid mt-2 skills">
@@ -88,12 +131,31 @@ const Allskills = () => {
           </div>
           <div className="col-md-4 mt-3">
             <em>Search Item </em>
-            {/* {data && data?.data.map((item) => {
-                          
-                      })} */}
-            <button className="btn btn-primary" onClick={Shuffleval}>
-              Shuffle
-            </button>
+            {car && car.length > 0 ? (
+              car?.map((item: any, ind: any) => {
+                return <p key={ind}>{item}</p>;
+              })
+            ) : (
+              <>"No Search Found"</>
+            )}
+            <>
+              <input
+                type="text"
+                value={finds}
+                placeholder="...Search Here"
+                onChange={(e: any) => setfinds(e.target.value)}
+              ></input>
+            </>
+          </div>
+          <div className="col-md-4 mt-3">
+            <em>file upload Item </em>
+            <input
+              type="file"
+              ref={refs}
+              accept="image/png,image/jpeg,image/jpg"
+              onChange={(e) => Fileupload(e)}
+            ></input>
+            <img src={binimg} className="img-fluid mt-2"></img>
           </div>
         </div>
       </div>
