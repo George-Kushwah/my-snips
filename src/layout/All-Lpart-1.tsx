fb1b8b60ea1665afaad71dd379624bbef0d7ea09
George <rahulsapnaji@gmail.com>
2025-03-22 18:42:41 +0530

    hello

--- src/layout/All-Lpart-1.tsx
+++ src/layout/All-Lpart-1.tsx
@@ -1,14 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Tableprint from "./Table-print";
 
 export default function AllLart1() {
+  const [adds, setadds] = useState<any>(0);
   const [vals, setVals] = useState<any>([{ value: "" }]);
   const [cols, setcols] = useState<any>("");
   const [rows, setrows] = useState<any>("");
   const [showTable, setshowTable] = useState<boolean>(false);
+  const [list1, setList1] = useState<any>([
+    { title: "item-1", checked: false },
+    { title: "item-2", checked: false },
+    { title: "item-3", checked: false },
+  ]);
+  const [list2, setList2] = useState<any>([
+    { title: "item-A", checked: false },
+    { title: "item-B", checked: false },
+    { title: "item-C", checked: false },
+  ]);
+  const [list3, setList3] = useState<any>([
+    { title: "item-1", checked: false },
+    { title: "item-2", checked: false },
+    { title: "item-3", checked: false },
+  ]);
+  const [list4, setList4] = useState<any>([
+    { title: "item-A", checked: false },
+    { title: "item-B", checked: false },
+    { title: "item-C", checked: false },
+  ]);
   const addfi = () => {
     setVals([...vals, { value: "" }]);
   };
+  // useEffect(() => {
+  //   setTimeout(() => setadds(adds + 1), 1000);
+  // });
 
   const Handlech = (inde: any, tar: any) => {
     const newfi = [...vals];
@@ -30,8 +54,56 @@ export default function AllLart1() {
     setshowTable(true);
   };
 
+  const Getswap = () => {
+    let lis1 = [...list1];
+    let lis2 = [...list2];
+    lis1.forEach((item: any, ind: any) => {
+      if (item.checked) {
+        const temp = lis1[ind].title;
+        lis1[ind].title = lis2[ind].title;
+        lis2[ind].title = temp;
+      }
+      lis1[ind].checked = false;
+    });
+    setList1(lis1);
+    setList2(lis2);
+  };
+
+  const Handlecheck = (ev: any) => {
+    const update = [...list1];
+    update[ev].checked = !update[ev].checked;
+    setList1(update);
+  };
+
+  const GetItemchecked1 = (ev: any) => {
+    let update = [...list3];
+    update[ev].checked = !update[ev].checked;
+    setList3(update);
+    let adlist1 = [...list4];
+    let adlist2 = [...list3];
+    adlist2[ev].checked = false;
+    adlist1.splice(adlist1.length, 0, update[ev]);
+    adlist2.splice(ev, 1);
+    setList4(adlist1);
+    setList3(adlist2);
+  };
+  const GetItemchecked2 = (ev: any) => {
+    let update = [...list4];
+    update[ev].checked = !update[ev].checked;
+    setList4(update);
+    let adlist1 = [...list3];
+    let adlist2 = [...list4];
+    adlist2[ev].checked = false;
+    adlist1.splice(adlist1.length, 0, update[ev]);
+    adlist2.splice(ev, 1);
+    //adlist1[ev].checked = false;
+    setList4(adlist2);
+    setList3(adlist1);
+  };
+
   return (
     <div className="container-fluid mt-3">
+      {adds}
       <div className="row">
         <div className="col-md-4">
           <form className="my-form-1" onSubmit={getval} id="myio">
@@ -88,6 +160,60 @@ export default function AllLart1() {
             </button>
           </form>
           {showTable && <Tableprint row={rows} col={cols} />}
+        </div>
+        <div className="col-md-4">
+          <ul>
+            {list1.map((item: any, ind: any) => (
+              <li key={ind}>
+                <input
+                  type="checkbox"
+                  checked={item.checked}
+                  onChange={() => Handlecheck(ind)}
+                ></input>
+                {item.title}
+              </li>
+            ))}
+          </ul>
+          <ul>
+            {list2.map((item: any, ind: any) => (
+              <li key={ind}>{item.title}</li>
+            ))}
+          </ul>
+          <button type="button" onClick={Getswap} className="btn btn-danger">
+            Swap Value
+          </button>
+        </div>
+        <div className="col-md-4">
+          <div className="row">
+            <div className="col-md-6">
+              <ul>
+                {list3.map((item: any, ind: any) => (
+                  <li key={`item1$${ind}`}>
+                    <input
+                      type="checkbox"
+                      checked={item.checked}
+                      onChange={() => GetItemchecked1(ind)}
+                    ></input>
+                    {item.title}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="col-md-6">
+              <ul>
+                {list4.map((item: any, ind: any) => (
+                  <li key={`item2$${ind}`}>
+                    <input
+                      type="checkbox"
+                      checked={item.checked}
+                      onChange={() => GetItemchecked2(ind)}
+                    ></input>
+                    {item.title}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
