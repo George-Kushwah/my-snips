89c86399a841915d415d7e071f5794f4cff03000
George <rahulsapnaji@gmail.com>
2025-05-01 14:37:30 +0530

    changes

--- src/Deep/Styles.tsx
+++ src/Deep/Styles.tsx
@@ -5,9 +5,20 @@ const Styles = () => {
   return (
     <div>
       {/* <button type="button">Click Me</button> */}
-      <Sbutton>Click Me</Sbutton>
+      {/* <Sbutton>Click Me</Sbutton>
       <br />
-      <Sbutton varient="outine">Click Me</Sbutton>
+      <Sbutton varient="outine">Click Me</Sbutton> */}
+
+      <form>
+        <label htmlFor="ing">Name</label>
+        <input
+          id="ing"
+          type="text"
+          placeholder="Enter value"
+          aria-label="Name"
+          aria-required="true"
+        ></input>
+      </form>
     </div>
   );
 };
